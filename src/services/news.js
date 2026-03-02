4e9f32ba196120723109add49988ba0840016036
Devapriya Anil <devapriyaanil373@gmail.com>
2026-03-02 14:15:45 +0530

    fixed news api

--- src/services/news.js
+++ src/services/news.js
@@ -1,22 +1,16 @@
 import axios from "axios";
 
-const NEWS_KEY = import.meta.env.VITE_NEWS_KEY;
-
 export const getEntertainmentNews = async () => {
   try {
-    const res = await axios.get("https://gnews.io/api/v4/top-headlines", {
-      params: {
-        category: "entertainment",
-        lang: "en",
-        country: "in",
-        max: 10,
-        apikey: NEWS_KEY,
-      },
-    });
+    const API_KEY = import.meta.env.VITE_NEWSDATA_KEY;
 
-    return res.data.articles;
+    const url = `https://newsdata.io/api/1/news?apikey=${API_KEY}&category=entertainment&country=in&language=en`;
+
+    const res = await axios.get(url);
+    return res.data.results || [];
+    
   } catch (error) {
-    console.error("GNews Error:", error);
+    console.error("NewsData Error:", error);
     return [];
   }
 };
