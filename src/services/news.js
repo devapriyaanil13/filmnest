3cc135b2ffb7b6955e449bcda248418b5e6cde90
Devapriya Anil <devapriyaanil373@gmail.com>
2026-03-02 14:33:17 +0530

    fixed news api

--- src/services/news.js
+++ src/services/news.js
@@ -7,8 +7,14 @@ export const getEntertainmentNews = async () => {
     const url = `https://newsdata.io/api/1/news?apikey=${API_KEY}&category=entertainment&country=in&language=en`;
 
     const res = await axios.get(url);
-    return res.data.results || [];
-    
+
+    return (res.data.results || []).map(item => ({
+      title: item.title,
+      description: item.description,
+      image: item.image_url || item.thumbnail || "https://via.placeholder.com/400x250?text=No+Image",
+      url: item.link
+    }));
+
   } catch (error) {
     console.error("NewsData Error:", error);
     return [];
